32c8afb59ac1508d269471020af27a7f50cc6949
汪帆 <sail032@live.cn>
2018-11-27 16:57:26 +0800

    初始化工程

---
+++ app/src/main/java/com/jinxin/beonesmartdesktop/activity/MainActivity.java
@@ -0,0 +1,22 @@
+package com.jinxin.beonesmartdesktop.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.jinxin.beonesmartdesktop.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText("");
+    }
+
+}
