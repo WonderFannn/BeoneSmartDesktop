adf52f7b0ddb304cd8bac62d937099579fb31389
汪帆 <sail032@live.cn>
2019-02-20 09:26:59 +0800

    正式版本

--- app/src/main/java/com/jinxin/beonesmartdesktop/fragment/DevicesFragment.java
+++ app/src/main/java/com/jinxin/beonesmartdesktop/fragment/DevicesFragment.java
@@ -6,12 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.jinxin.beonesmartdesktop.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class DevicesFragment extends Fragment {
+
+    @BindView(R.id.lv_gateway) ListView mLvGateway;
+    @BindView(R.id.lv_devices) ListView mLvDevices;
+
 
     @Nullable
     @Override
@@ -20,5 +27,15 @@ public class DevicesFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_devices, container, false);
         ButterKnife.bind(this, view);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter<String> adapter1 =new ArrayAdapter<String>(getActivity(),R.layout.listview_item,getResources().getStringArray(R.array.names));
+        ArrayAdapter<String> adapter2 =new ArrayAdapter<String>(getActivity(),R.layout.listview_item,getResources().getStringArray(R.array.names2));
+
+        mLvGateway.setAdapter(adapter1);
+        mLvDevices.setAdapter(adapter2);
     }
 }
